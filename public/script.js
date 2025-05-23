1a5cb79ce93dca144b93c5d1290cf2c5da496eba
MohdRavishIdreesi <mohd.ravish.idreesi@gmail.com>
2025-05-23 17:20:42 +0000

    front end code upgdated

--- public/script.js
+++ public/script.js
@@ -1,22 +1,21 @@
 document.getElementById('feedbackForm').addEventListener('submit', async function (e) {
-    e.preventDefault();
-  
-    const formData = {
-      name: this.name.value,
-      email: this.email.value,
-      message: this.message.value
-    };
-  
-    const res = await fetch('/submit', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData)
-    });
-  
-    const data = await res.json();
-    document.getElementById('response').textContent = data.message;
-    this.reset();
+  e.preventDefault();
+
+  const formData = {
+    name: this.name.value,
+    email: this.email.value,
+    message: this.message.value
+  };
+
+  const res = await fetch('/submit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
   });
-  
+
+  const data = await res.json();
+  document.getElementById('response').textContent = data.message;
+  this.reset();
+});
